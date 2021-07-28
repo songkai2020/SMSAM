cad8bcf3e093b9e2088afe65caeb74f064631fe2
Rookie DD <61354006+songkai2020@users.noreply.github.com>
2021-07-28 15:31:33 +0800

    Create glcm.py

---
+++ Data_process/glcm.py
@@ -0,0 +1,206 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+from PIL import Image
+from skimage import data
+from math import floor, ceil
+from skimage.feature import greycomatrix, greycoprops
+import os
+import cv2
+
+def image_patch(img2, slide_window, h, w):
+
+    image = img2
+    window_size = slide_window
+    patch = np.zeros((slide_window, slide_window, h, w), dtype=np.uint8)
+
+    for i in range(patch.shape[2]):
+        for j in range(patch.shape[3]):
+            patch[:, :, i, j] = img2[i : i + slide_window, j : j + slide_window]
+ 
+    return patch
+
+def calcu_glcm(img, vmin=0, vmax=255, nbit=64, slide_window=5, step=[2], angle=[0]):
+        
+    mi, ma = vmin, vmax
+    h, w = img.shape
+
+    # Compressed gray range：vmin: 0-->0, vmax: 256-1 -->nbit-1
+    bins = np.linspace(mi, ma+1, nbit+1)
+    img1 = np.digitize(img, bins) - 1
+
+    # (512, 512) --> (slide_window, slide_window, 512, 512)
+    img2 = cv2.copyMakeBorder(img1, floor(slide_window/2), floor(slide_window/2)
+                              , floor(slide_window/2), floor(slide_window/2), cv2.BORDER_REPLICATE) # 图像扩充
+
+    patch = np.zeros((slide_window, slide_window, h, w), dtype=np.uint8)
+    patch = image_patch(img2, slide_window, h, w)
+
+    # Calculate GLCM (5, 5, 512, 512) --> (64, 64, 512, 512)
+    # greycomatrix(image, distances, angles, levels=None, symmetric=False, normed=False)
+    glcm = np.zeros((nbit, nbit, len(step), len(angle), h, w), dtype=np.uint8)
+    for i in range(patch.shape[2]):
+        for j in range(patch.shape[3]):
+            glcm[:, :, :, :, i, j]= greycomatrix(patch[:, :, i, j], step, angle, levels=nbit)
+
+    return glcm
+
+def calcu_glcm_mean(glcm, nbit=64):
+    '''
+    calc glcm mean
+    '''
+    mean = np.zeros((glcm.shape[2], glcm.shape[3]), dtype=np.float32)
+    for i in range(nbit):
+        for j in range(nbit):
+            mean += glcm[i,j] * i / (nbit)**2
+
+    return mean
+
+def calcu_glcm_variance(glcm, nbit=64):
+    '''
+    calc glcm variance
+    '''
+    mean = np.zeros((glcm.shape[2], glcm.shape[3]), dtype=np.float32)
+    for i in range(nbit):
+        for j in range(nbit):
+            mean += glcm[i, j] * i / (nbit)**2
+
+    variance = np.zeros((glcm.shape[2], glcm.shape[3]), dtype=np.float32)
+    for i in range(nbit):
+        for j in range(nbit):
+            variance += glcm[i, j] * (i - mean)**2
+
+    return variance
+
+def calcu_glcm_homogeneity(glcm, nbit=64):
+    '''
+    calc glcm Homogeneity
+    '''
+    Homogeneity = np.zeros((glcm.shape[2], glcm.shape[3]), dtype=np.float32)
+    for i in range(nbit):
+        for j in range(nbit):
+            Homogeneity += glcm[i,j] / (1.+(i-j)**2)
+
+    return Homogeneity
+
+def calcu_glcm_contrast(glcm, nbit=64):
+    '''
+    calc glcm contrast
+    '''
+    contrast = np.zeros((glcm.shape[2], glcm.shape[3]), dtype=np.float32)
+    for i in range(nbit):
+        for j in range(nbit):
+            contrast += glcm[i, j] * (i-j)**2
+
+    return contrast
+
+def calcu_glcm_dissimilarity(glcm, nbit=64):
+    '''
+    calc glcm dissimilarity
+    '''
+    dissimilarity = np.zeros((glcm.shape[2], glcm.shape[3]), dtype=np.float32)
+    for i in range(nbit):
+        for j in range(nbit):
+            dissimilarity += glcm[i, j] * np.abs(i-j)
+
+    return dissimilarity
+
+def calcu_glcm_entropy(glcm, nbit=64):
+    '''
+    calc glcm entropy 
+    '''
+    eps = 0.00001
+    entropy = np.zeros((glcm.shape[2], glcm.shape[3]), dtype=np.float32)
+    for i in range(nbit):
+        for j in range(nbit):
+            entropy -= glcm[i, j] * np.log10(glcm[i, j] + eps)
+
+    return entropy
+
+def calcu_glcm_energy(glcm, nbit=64):
+    '''
+    calc glcm energy or second moment
+    '''
+    energy = np.zeros((glcm.shape[2], glcm.shape[3]), dtype=np.float32)
+    for i in range(nbit):
+        for j in range(nbit):
+            energy += glcm[i, j]**2
+
+    return energy
+
+def calcu_glcm_correlation(glcm, nbit=64):
+    '''
+    calc glcm correlation (Unverified result)
+    '''
+    
+    mean = np.zeros((glcm.shape[2], glcm.shape[3]), dtype=np.float32)
+    for i in range(nbit):
+        for j in range(nbit):
+            mean += glcm[i, j] * i / (nbit)**2
+
+    variance = np.zeros((glcm.shape[2], glcm.shape[3]), dtype=np.float32)
+    for i in range(nbit):
+        for j in range(nbit):
+            variance += glcm[i, j] * (i - mean)**2
+    
+    correlation = np.zeros((glcm.shape[2], glcm.shape[3]), dtype=np.float32)
+    for i in range(nbit):
+        for j in range(nbit):
+            correlation += ((i - mean) * (j - mean) * (glcm[i, j]**2))/variance
+
+    return correlation
+
+def calcu_glcm_Auto_correlation(glcm, nbit=64):
+    '''
+    calc glcm auto correlation
+    '''
+    Auto_correlation = np.zeros((glcm.shape[2], glcm.shape[3]), dtype=np.float32)
+    for i in range(nbit):
+        for j in range(nbit):
+            Auto_correlation += glcm[i, j] * i * j
+
+    return Auto_correlation
+
+if __name__ == '__main__':
+    
+    nbit = 64 # gray levels
+    mi, ma = 0, 255 # max gray and min gray
+    slide_window = 7 # sliding window
+    # step = [2, 4, 8, 16] # step
+    # angle = [0, np.pi/4, np.pi/2, np.pi*3/4] # angle or direction
+    step = [2]
+    angle = [0]
+    #original image path
+    path1='/content/sample_data/rec_total_16'
+    #save path
+    path2='/content/sample_data/glcm_entropy'
+    imgs = os.listdir(path1)
+    for img_ in imgs:
+      image = path1+'/'+img_
+      img = np.array(Image.open(image)) # If the image has multi-bands, it needs to be converted to grayscale image
+      img = np.uint8(255.0 * (img - np.min(img))/(np.max(img) - np.min(img))) # normalization
+      h, w = img.shape
+    
+      glcm = calcu_glcm(img, mi, ma, nbit, slide_window, step, angle)
+
+      # 
+      for i in range(glcm.shape[2]):
+        for j in range(glcm.shape[3]):
+          glcm_cut = np.zeros((nbit, nbit, h, w), dtype=np.float32)
+          glcm_cut = glcm[:, :, i, j, :, :]
+          # mean = calcu_glcm_mean(glcm_cut, nbit)
+          # variance = calcu_glcm_variance(glcm_cut, nbit)
+          # homogeneity = calcu_glcm_homogeneity(glcm_cut, nbit)
+          # contrast = calcu_glcm_contrast(glcm_cut, nbit)
+          # dissimilarity = calcu_glcm_dissimilarity(glcm_cut, nbit)
+          entropy = calcu_glcm_entropy(glcm_cut, nbit)
+          # energy = calcu_glcm_energy(glcm_cut, nbit)
+          # correlation = calcu_glcm_correlation(glcm_cut, nbit)
+          # Auto_correlation = calcu_glcm_Auto_correlation(glcm_cut, nbit)
+      #np.savetxt(path2+'/'+img_.split('.')[0]+'.txt', entropy, fmt="%f", delimiter=" ")
+      np.save(path2+'/'+img_.split('.')[0]+'.txt', entropy)
+
+
+
+
+    
